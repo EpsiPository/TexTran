527cccf53b8202d3660925c15a3e7a84eec5d2d0
Tim Maes <tim.maes@sweetmustard.be>
2019-03-25 16:22:53 +0100

    Add project files.

---
+++ TextTransformation/Templates/Enums.cs
@@ -0,0 +1,26 @@
+﻿
+
+
+/////////////////////////////////////////////////////////////////////
+//This code is auto-generated. Changes to this file will be lost! //
+/////////////////////////////////////////////////////////////////////
+using System;
+
+namespace TextTransformation.TextTransformation.Enums
+{
+
+	public enum Gender
+	{
+ 	 Male = 1,
+ 	 Female = 2,
+ 	 Transypansy = 3,
+ 	 Unknown = 4,
+ 	}
+
+	public enum Role
+	{
+ 	 Member = 1,
+ 	 Admin = 2,
+ 	 Loser = 3,
+ 	}
+}
---
+++ TextTransformation/Templates/Models.cs
@@ -0,0 +1,27 @@
+﻿
+
+/////////////////////////////////////////////////////////////////////
+//This code is auto-generated. Changes to this file will be lost! //
+/////////////////////////////////////////////////////////////////////
+using System;
+namespace TextTransformation.TextTransformation.Models
+{
+	public class User
+	{
+		public Guid Id { get; set; }
+
+		public string Name { get; set; }
+
+		public string Email { get; set; }
+
+	}
+
+	public class Role
+	{
+		public Guid Id { get; set; }
+
+		public string Name { get; set; }
+
+	}
+
+}
