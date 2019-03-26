d8a5dbd947bd06674920b371047083671bf2b52f
Tim Maes <tim.maes@sweetmustard.be>
2019-03-26 15:41:38 +0100

    Generate DbContext

---
+++ src/TexTran.Data.Access/Context/TexTranContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using TexTran.Data.Abstractions.Entities;
+namespace TextTran.Data.Access.Context
+{
+	public class TextTranContext : DbContext
+	{
+		public TextTranContext(DbContextOptions options) : base(options) { }
+
+		public DbSet<Order> Orders { get; set; }
+		public DbSet<Product> Products { get; set; }
+		public DbSet<User> Users { get; set; }
+	}
+}
+
