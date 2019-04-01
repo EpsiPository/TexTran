0899e052e3e9a4773a4effa8236b96cd790b9255
Tim Maes <tim.maes@sweetmustard.be>
2019-04-01 09:12:25 +0200

    Remove Transformation project

---
+++ src/TexTran.Data.Abstractions/Templates/Enums.cs
@@ -0,0 +1,22 @@
+﻿
+
+using System;
+
+namespace TextTran.Transformations.Enums
+{
+
+	public enum Gender
+	{
+ 	 Male = 1,
+ 	 Female = 2,
+ 	 Transypansy = 3,
+ 	 Unknown = 4,
+ 	}
+
+	public enum Role
+	{
+ 	 Member = 1,
+ 	 Admin = 2,
+ 	 Loser = 3,
+ 	}
+}
